bd512a92a990781ba0b9001ed90691cadcadd153
Jacob Lockett <jacobwyattlockett@gmail.com>
2025-10-22 16:47:36 -0400

    added redirect route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import Router, { Switch, Route } from "crossroad"
+import { useEffect } from "react"
 import Welcome from "./pages/Welcome"
 import Install from "./pages/Install"
 import Agree from "./pages/Agree"
 import Unsupported from "./pages/Unsupported"
 import NotFound from "./pages/NotFound"
+import { useParams, usePath } from "crossroad"
+
+function Redirect() {
+	const params = useParams("/:type/:code")
+	const [, setPath] = usePath()
+
+	useEffect(() => {
+		setPath(`/${params.type}/${params.code}/welcome`)
+	}, [params])
+
+	return null
+}
 
 function App() {
 	return (
 		<Router>
 			<Switch>
+				<Route path="/:type/:code" component={Redirect} />
 				<Route path="/:type/:code/welcome" component={Welcome} />
 				<Route path="/:type/:code/install" component={Install} />
 				<Route path="/:type/:code/agree" component={Agree} />
